7573499617c8373e71e60cbc15c8954d0b96d165
Ynda Jas <yndajas@gmail.com>
2021-02-27 01:43:54 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :users
+
+  get 'signin' => 'sessions#new'
+  resources :sessions, only: [:create]
+  delete 'session' => 'sessions#destroy'
+
+  resources :attractions
+
+  resources :rides, only: [:create]
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
